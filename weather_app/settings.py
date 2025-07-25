4755d9ccf6e0c3e5824491828f6e377289945ea9
Yaswanth babu Patnam <yaswanthpatnam1819@gmail.com>
2025-07-25 17:18:51 +0530

    Add Render domain to ALLOWED_HOSTS

--- weather_app/settings.py
+++ weather_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)ey_xcif5&y^89bu5f9vq)5^p33csm&0(^-%qz81)o4(66ssbr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'weather-app-b2sm.onrender.com']
 
 
 # Application definition
