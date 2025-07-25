7a6efd0c35e50f1c9d7e873e9c44f5fdd083d2fe
Yaswanth babu Patnam <yaswanthpatnam1819@gmail.com>
2025-07-25 17:58:03 +0530

    updated settings.py

--- weather_app/settings.py
+++ weather_app/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,10 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'weather/static')
+    os.path.join(BASE_DIR, 'weather/static/css/style.css')
 ]
 
 # Default primary key field type
